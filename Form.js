1f393f42801045263f403f6d2655731135307b09
oscarhasanp <oscarhasanputra1@gmail.com>
2020-09-14 16:13:22 +0700

    update animasi bg dan mengubah form

--- Form.js
+++ Form.js
@@ -1,15 +1,21 @@
-$('#form').click((e)=>{
+var bg=1
+$('#animateBtn').click((e)=>{
     e.preventDefault()
-    
-    const form =$(".form")
-$('#form').css({"transition":'all 2s',"transform":"translate(-100%,0)"})
+    const form =$(".form-animative")
+    form.map((index,elem)=>{
+        console.log("aku dulu")
+        const translate=$(elem).css("transform")
+        const xTrigger=parseInt(translate.split(",")[4])
+        const x=xTrigger==0?((index*(-200))+100+"%"):0;
+        $(elem).css({"transition":'all 2s',"transform":`translate(${x},0)`})
+    })
+    bg=bg==2?1:2
+    $('#form2').css({"background-image":`url('gambar${bg}.jpg')`})
+// $('#form').css({"transition":'all 2s',"transform":"translate(-100%,0)"})
 
 
-$('#form2').css({"transition":'all 2s',"transform":"translate(100%,0)"})
+// $('#form2').css({"transition":'all 2s',"transform":"translate(100%,0)"})
 // $(e.target).css({"-webkit-transform":'translate(-100,0)'})
 console.log($('#form2'))
 
-// $('#form2').animate({
-//     right:'0',
-// },1000)
 })
