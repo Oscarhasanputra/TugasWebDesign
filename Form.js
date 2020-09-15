778382defa2a64b971e66b52b8cac102190fadce
oscarhasanp <oscarhasanputra1@gmail.com>
2020-09-15 14:04:47 +0700

    update

--- Form.js
+++ Form.js
@@ -1,21 +1,62 @@
 var bg=1
-$('#animateBtn').click((e)=>{
-    e.preventDefault()
+var windowResized=false;
+window.addEventListener('resize',()=>{
+    
+    const width=document.body.clientWidth
+    // console.log(width)   
+    windowResized=width<=995?(windowResized==false?true:false):(windowResized==true?true:false)
+    console.log(windowResized)
+    if(windowResized){
+        console.log("resized")
     const form =$(".form-animative")
     form.map((index,elem)=>{
-        console.log("aku dulu")
+        $(elem).css({"transition":'none',"transform":`translate(0,0)`})
+    })
+    }
+})
+$('#animateBtn').click((e)=>{
+    e.preventDefault()
+    const width=document.body.clientWidth
+    const form =$(".form-animative")
+    form.map((index,elem)=>{
         const translate=$(elem).css("transform")
-        const xTrigger=parseInt(translate.split(",")[4])
-        const x=xTrigger==0?((index*(-200))+100+"%"):0;
-        $(elem).css({"transition":'all 2s',"transform":`translate(${x},0)`})
+        let x=0;
+        
+        if(width>995){
+            
+                 const xTrigger=parseInt(translate.split(",")[4])
+                x=xTrigger==0?((index*(-200))+100+"%"):0;
+                $(elem).css({"transition":'all 2s',"transform":`translate(${x},0)`})
+        }
+           
+        else {
+            const xTrigger=parseInt(translate.split(",")[5])
+            x=xTrigger==0?((index*(-200))+100):0;
+            $(elem).css({"transition":'all 2s',"transform":`translate(0,${x}%)`})
+        }
+
     })
     bg=bg==2?1:2
     $('#form2').css({"background-image":`url('gambar${bg}.jpg')`})
+    const targetChanged=$(".formtest")
+    setTimeout(()=>{
+        targetChanged.map((index,elem)=>{
+            const classStr=elem.className
+            const isFound=classStr.indexOf("d-none")
+            if(isFound>=0){
+                $(elem).removeClass("d-none")
+            }else{
+                $(elem).addClass("d-none")
+            }
+        })
+    },600)
+  
+    
 // $('#form').css({"transition":'all 2s',"transform":"translate(-100%,0)"})
 
 
 // $('#form2').css({"transition":'all 2s',"transform":"translate(100%,0)"})
 // $(e.target).css({"-webkit-transform":'translate(-100,0)'})
-console.log($('#form2'))
+// console.log($('#form2'))
 
 })
